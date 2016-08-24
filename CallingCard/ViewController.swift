c3d02ac143e131247ed8c9a9641473013fe9f915
Jake Alsemgeest <alsemgeester@gmail.com>
2016-08-24 19:21:18 -0400

    Initial Commit

---
+++ CallingCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CallingCard
+//
+//  Created by Jake Alsemgeest on 2016-08-24.
+//  Copyright © 2016 Jalsemgeest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
